e42cbddf832682f2329fef44f91b6f3977f7e79c
Jeremy <jrjn71780@gmail.com>
2025-08-12 00:18:34 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
-#!/usr/bin/env python3
+def print_fibonacci(n):
+    fib = [0, 1]  # Starting numbers of the Fibonacci sequence
+    if n <= 0:
+        print([])  # Print empty list for non-positive n
+        return
+    elif n == 1:
+        print([0])
+        return
 
-def print_fibonacci(length):
-    pass
+    # Generate Fibonacci numbers up to length n
+    while len(fib) < n:
+        next_num = fib[-1] + fib[-2]  # Sum of last two numbers
+        fib.append(next_num)
+
+    print(fib[:n])
+print_fibonacci(9)
